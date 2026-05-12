1f0d018fd797a58bbc8e6d58282a2e8571969965
Modemola <sinaakejudaniel@gmail.com>
2026-05-12 12:57:51 +0100

    Refactor code structure for improved readability and maintainability

---
+++ server/SRC/index.js
@@ -0,0 +1,14 @@
+// index.js
+import express from 'express';
+
+const app = express();
+const PORT = 3010;
+
+app.get('/', (req, res) => {
+  res.send('Hello from ROYAL STACK');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running at  http://localhost:${PORT}`);
+});
